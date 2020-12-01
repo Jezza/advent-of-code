4085510a250a06aec043fd70a62cd60f2f271d47
Jezza <jezzadabomb@gmail.com>
2020-12-01 13:56:35 +0100

    Separate the search code into their own functions.
    I'm wondering if the for loop itself is worth separating again.
    Maybe behind a macro...
    Probably not worth it...

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -8,29 +8,43 @@ fn main() {
 fn try_main() -> Result<(), Box<dyn std::error::Error>> {
 	let numbers = read_numbers()?;
 
-	'part_one:
-	for (l_i, l) in numbers.iter().enumerate() {
-		for (r_i, r) in numbers.iter().enumerate() {
+	if let Some((l, r)) = find_pair(&numbers) {
+		println!("Part One: {:>4} * {:>4}        = {}", l, r, l * r);
+	} else {
+		eprintln!("Unable to find pair.");
+	}
+
+	if let Some((l, m, r)) = find_triple(&numbers) {
+		println!("Part Two: {:>4} * {:>4} * {:>4} = {}", l, m, r, l * m * r);
+	} else {
+		eprintln!("Unable to find triple.");
+	}
+
+	Ok(())
+}
+
+fn find_pair(input: &[u32]) -> Option<(u32, u32)> {
+	for (l_i, l) in input.iter().enumerate() {
+		for (r_i, r) in input.iter().enumerate() {
 			if r_i > l_i && *l + *r == 2020 {
-				println!("Part One: {}, {} = {}", l, r, l * r);
-				break 'part_one;
+				return Some((*l, *r));
 			}
 		}
 	}
+	None
+}
 
-	'part_two:
-	for (l_i, l) in numbers.iter().enumerate() {
-		for (m_i, m) in numbers.iter().enumerate() {
-			for (r_i, r) in numbers.iter().enumerate() {
+fn find_triple(input: &[u32]) -> Option<(u32, u32, u32)> {
+	for (l_i, l) in input.iter().enumerate() {
+		for (m_i, m) in input.iter().enumerate() {
+			for (r_i, r) in input.iter().enumerate() {
 				if r_i > m_i && m_i > l_i && *l + *m + *r == 2020 {
-					println!("Part Two: {} * {} * {} = {}", l, m, r, *l * *m * *r);
-					break 'part_two;
+					return Some((*l, *m, *r));
 				}
 			}
 		}
 	}
-
-	Ok(())
+	None
 }
 
 fn read_numbers() -> Result<Vec<u32>, std::num::ParseIntError> {
