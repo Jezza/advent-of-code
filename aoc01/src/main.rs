5dc3739722773913a606ba739ea02b3d791b71bb
Jezza <jezzadabomb@gmail.com>
2020-12-01 13:41:36 +0100

    Check the index to make sure we're comparing different numbers.

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
 	if let Err(err) = try_main() {
-		eprintln!("Application experienced an error: {}", err);
+		eprintln!("Error: {}", err);
 		std::process::exit(1);
 	}
 }
@@ -9,21 +9,21 @@ fn try_main() -> Result<(), Box<dyn std::error::Error>> {
 	let numbers = read_numbers()?;
 
 	'part_one:
-	for l in &numbers {
-		for r in &numbers {
-			if *l + *r == 2020 {
-				println!("Part One: {}", l * r);
+	for (l_i, l) in numbers.iter().enumerate() {
+		for (r_i, r) in numbers.iter().enumerate() {
+			if r_i > l_i && *l + *r == 2020 {
+				println!("Part One: {}, {} = {}", l, r, l * r);
 				break 'part_one;
 			}
 		}
 	}
 
 	'part_two:
-	for l in &numbers {
-		for m in &numbers {
-			for r in &numbers {
-				if *l + *m + *r == 2020 {
-					println!("Part Two: {}", *l * *m * *r);
+	for (l_i, l) in numbers.iter().enumerate() {
+		for (m_i, m) in numbers.iter().enumerate() {
+			for (r_i, r) in numbers.iter().enumerate() {
+				if r_i > m_i && m_i > l_i && *l + *m + *r == 2020 {
+					println!("Part Two: {} * {} * {} = {}", l, m, r, *l * *m * *r);
 					break 'part_two;
 				}
 			}
