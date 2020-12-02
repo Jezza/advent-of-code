efb38be09d177bbccebc1d141ed64d723fe22941
Jezza <jezzadabomb@gmail.com>
2020-12-02 13:05:33 +0100

    Don't need to use 'static lifetimes here.

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -15,11 +15,11 @@ fn part_one() -> usize {
 }
 
 fn part_two() -> usize {
-	let handler = |pos_one, pos_two, char, password: &'static [u8]| (password[pos_one - 1] == char) ^ (password[pos_two - 1] == char);
+	let handler = |pos_one, pos_two, char, password: &'_ [u8]| (password[pos_one - 1] == char) ^ (password[pos_two - 1] == char);
 	process_inputs(handler)
 }
 
-fn process_inputs(mut handler: impl FnMut(usize, usize, u8, &'static [u8]) -> bool) -> usize {
+fn process_inputs(mut handler: impl FnMut(usize, usize, u8, &'_ [u8]) -> bool) -> usize {
 	let input = include_str!("../input/input.txt");
 
 	let regex = regex::RegexBuilder::new(r"(\d+)\-(\d+) (\w): (\w+?)$")
