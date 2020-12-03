694e8928dfba5f502ba6d41b8366fd37e811080b
Jezza <jezzadabomb@gmail.com>
2020-12-03 14:18:50 +0100

    Make Day 3 more confusing.
    I forgot that I could cycle iterators, which includes ranges.

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -1,3 +1,68 @@
+
+fn main() {
+	println!("Part One: {}", part_one());
+	println!("Part Two: {}", part_two());
+}
+
+fn part_one() -> usize {
+	follow_steps(&[
+		(3, 1)
+	])
+}
+
+fn part_two() -> usize {
+	follow_steps(&[
+		(1, 1),
+		(3, 1),
+		(5, 1),
+		(7, 1),
+		(1, 2),
+	])
+}
+
+fn follow_steps(steps: &[(usize, usize)]) -> usize {
+	const INPUT: &str = include_str!("../input/input.txt");
+	let width = INPUT.find('\n').unwrap();
+
+	steps.iter()
+		.fold(1, |acc, step| acc * INPUT.lines()
+			.step_by(step.1)
+			.zip((0..width)
+				.cycle()
+				.step_by(step.0))
+			.skip(1)
+			.filter(|&(line, x_pos)| line.bytes()
+				.nth(x_pos)
+				.map(|c| c == b'#')
+				.unwrap_or(false))
+			.count())
+}
+
+fn follow_steps_structural(steps: &[(usize, usize)]) -> usize {
+	let map = &Map::new();
+
+	steps.iter()
+		.fold(1, |acc, step| {
+			let mut pos = (0, 0);
+			let mut count = 0;
+
+			loop {
+				pos.0 += step.0;
+				pos.1 += step.1;
+
+				if let Some(value) = map.at(pos) {
+					if value == '#' {
+						count += 1;
+					}
+				} else {
+					break;
+				}
+			}
+
+			acc * count
+		})
+}
+
 struct Map {
 	width: usize,
 	data: Box<[u8]>,
@@ -24,49 +89,4 @@ impl Map {
 			.cloned()
 			.map(|c| c as char)
 	}
-}
-
-fn main() {
-	let map = &Map::new();
-
-	println!("Part One: {}", part_one(map));
-	println!("Part Two: {}", part_two(map));
-}
-
-fn part_one(map: &Map) -> usize {
-	const STEP: (usize, usize) = (3, 1);
-
-	follow_steps(map, STEP)
-}
-
-fn part_two(map: &Map) -> usize {
-	vec![
-		(1, 1),
-		(3, 1),
-		(5, 1),
-		(7, 1),
-		(1, 2),
-	].into_iter()
-		.map(|step| follow_steps(map, step))
-		.product()
-}
-
-fn follow_steps(map: &Map, step: (usize, usize)) -> usize {
-	let mut pos = (0, 0);
-	let mut count = 0;
-
-	loop {
-		pos.0 += step.0;
-		pos.1 += step.1;
-
-		if let Some(value) = map.at(pos) {
-			if value == '#' {
-				count += 1;
-			}
-		} else {
-			break;
-		}
-	}
-
-	count
 }
