35b55895218c4cffd9824d1a7ab1a8b8724ec216
Jezza <jezzadabomb@gmail.com>
2020-12-04 17:34:50 +0100

    Make implementation more confusing.

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -2,48 +2,34 @@
 
 use std::collections::HashMap;
 
+const FIELDS: &[(&'static str, fn(&'static str) -> bool)] = {
+	#[inline(always)]
+	fn check_range(value: &str, range: std::ops::RangeInclusive<usize>) -> bool {
+		range.contains(&value.parse::<usize>().unwrap())
+	}
+
+	&[
+		("byr", |value| check_range(value, 1920..=2002)),
+		("iyr", |value| check_range(value, 2010..=2020)),
+		("eyr", |value| check_range(value, 2020..=2030)),
+		("hgt", |value| check_range(&value[..value.len() - 2], if value.ends_with("cm") { 150..=193 } else { 59..=76 })),
+		("hcl", |value| value.starts_with("#") && value.as_bytes().iter().skip(1).all(|c| b'0' <= *c && *c <= b'9' || b'a' <= *c && *c <= b'z')),
+		("ecl", |value| matches!(value, "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth")),
+		("pid", |value| value.len() == 9 && value.as_bytes().iter().all(|c| b'0' <= *c && *c <= b'9')),
+	]
+};
+
 fn main() {
 	println!("Part One: {}", part_one());
 	println!("Part Two: {}", part_two());
 }
 
 fn part_one() -> usize {
-	check_details(|opts| opts.contains_key("byr")
-		&& opts.contains_key("iyr")
-		&& opts.contains_key("eyr")
-		&& opts.contains_key("hgt")
-		&& opts.contains_key("hcl")
-		&& opts.contains_key("ecl")
-		&& opts.contains_key("pid"))
+	check_details(|passport| FIELDS.iter().all(|(value, _)| passport.contains_key(value)))
 }
 
 fn part_two() -> usize {
-	let check_range = |value: &str, range: std::ops::RangeInclusive<usize>| range.contains(&value.parse::<usize>().unwrap());
-
-	// a # followed by exactly six characters 0-9 or a-f.
-	let check_hex = |input: &str|
-		input.starts_with("#") && input.as_bytes()
-			.iter()
-			.skip(1)
-			.all(|c| b'0' <= *c && *c <= b'9' || b'a' <= *c && *c <= b'z');
-
-	let check_eyes = |input: &str| matches!(input, "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth");
-
-	// a nine-digit number, including leading zeroes.
-	let check_passport = |input: &str|
-		input.len() == 9 && input.as_bytes()
-			.iter()
-			.all(|c| b'0' <= *c && *c <= b'9');
-
-	check_details(|opts| {
-		opts.get("byr").map_or(false, |value| check_range(value, 1920..=2002))
-			&& opts.get("iyr").map_or(false, |&value| check_range(value, 2010..=2020))
-			&& opts.get("eyr").map_or(false, |&value| check_range(value, 2020..=2030))
-			&& opts.get("hgt").map_or(false, |&value| check_range(&value[..value.len() - 2], if value.ends_with("cm") { 150..=193 } else { 59..=76 }))
-			&& opts.get("hcl").map_or(false, |&value| check_hex(value))
-			&& opts.get("ecl").map_or(false, |&value| check_eyes(value))
-			&& opts.get("pid").map_or(false, |&value| check_passport(value))
-	})
+	check_details(|passport| FIELDS.iter().all(|&(value, func)| passport.get(value).map_or(false, |&f| func(f))))
 }
 
 fn check_details(handler: impl Fn(&HashMap<&'static str, &'static str>) -> bool) -> usize {
