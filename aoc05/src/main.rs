6343d8b346d0f247a303cbd892f8621d128210bd
Jezza <jezzadabomb@gmail.com>
2020-12-05 11:40:36 +0100

    Prepare the array before calculations.
    Dirk was doing it, and I wanted to compare.

--- aoc05/src/main.rs
+++ aoc05/src/main.rs
@@ -1,7 +1,33 @@
+#![feature(test)]
+
+extern crate test;
+
+macro_rules! measure {
+    ($expr:expr) => {{
+    	let stats = test::bench::iter(&mut || $expr);
+		let median = stats.median as usize;
+		let deviation = (stats.max - stats.min) as usize;
+		println!("test {:<36}\tbench:\t{:>11} ns/iter (+/- {})", stringify!($expr), median, deviation);
+		$expr
+    }};
+}
 
 fn main() {
-	println!("Part One: {}", part_one());
-	println!("Part Two: {}", part_two());
+	{
+		println!("Part One: {}", measure!(part_one()));
+		println!("Part Two: {}", measure!(part_two()));
+	}
+
+	{
+		let input = measure! {{
+			let mut input = read_ids().collect::<Vec<usize>>();
+			input.sort_unstable();
+			input
+		}};
+
+		println!("Part One: {}", measure!(part_one_prepared(&input)));
+		println!("Part Two: {}", measure!(part_two_prepared(&input)));
+	}
 }
 
 fn part_one() -> usize {
@@ -20,10 +46,21 @@ fn part_two() -> usize {
 		.unwrap()[0] + 1usize
 }
 
+fn part_one_prepared(data: &[usize]) -> usize {
+	*data.last().unwrap()
+}
+
+fn part_two_prepared(data: &[usize]) -> usize {
+	data.windows(2)
+		.find(|slice| slice[0] + 1 != slice[1])
+		.unwrap()[0] + 1usize
+}
+
 fn read_ids() -> impl Iterator<Item = usize> {
 	include_str!("../input/input.txt")
 		.lines()
-		.map(|l| l.as_bytes().iter().fold(0, |acc, half| {
-			(acc << 1) | if *half == b'B' || *half == b'R' { 1 } else { 0 }
-		}))
+		.map(|l| l.as_bytes()
+			.iter()
+			.map(|c| if *c == b'B' || *c == b'R' { 1 } else { 0 })
+			.fold(0, |acc, value| (acc << 1) | value))
 }
