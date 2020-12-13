06d4d57887fd4f06fce268803643acec7cb9bdba
Jezza <jezzadabomb@gmail.com>
2020-12-13 20:42:36 +0100

    Cleanup Day 12.

--- aoc12/src/main.rs
+++ aoc12/src/main.rs
@@ -1,11 +1,10 @@
 use helper::measure;
-use std::ops::Rem;
 
 fn main() {
-	// println!("Part One: {}", measure!(part_one()));
-	// println!("Part Two: {}", measure!(part_two()));
-	println!("Part One: {}", part_one());
-	println!("Part Two: {}", part_two());
+	println!("Part One: {}", measure!(part_one()));
+	println!("Part Two: {}", measure!(part_two()));
+	// println!("Part One: {}", part_one());
+	// println!("Part Two: {}", part_two());
 }
 
 #[derive(Debug)]
@@ -19,139 +18,65 @@ enum Command {
 	Forward(isize),
 }
 
-fn part_one() -> usize {
-	let commands = include_str!("../input/input.txt")
+fn parse_input() -> impl Iterator<Item = Command> {
+	include_str!("../input/input.txt")
 		.lines()
-		.map(|line| {
-
-			let (cmd, value) = line.split_at(1);
-			let value = value.parse::<isize>().unwrap();
-
-			match cmd {
-				"N" => Command::North(value),
-				"S" => Command::South(value),
-				"E" => Command::East(value),
-				"W" => Command::West(value),
-				"L" => Command::Left(value),
-				"R" => Command::Right(value),
-				"F" => Command::Forward(value),
-				_ => panic!("Unsupported command: {:?}", cmd),
-			}
+		.map(|line| line.split_at(1))
+		.map(|(cmd, value)| (cmd, value.parse::<isize>().unwrap()))
+		.map(|(cmd, value)| match cmd {
+			"N" => Command::North(value),
+			"S" => Command::South(value),
+			"E" => Command::East(value),
+			"W" => Command::West(value),
+			"L" => Command::Left(value),
+			"R" => Command::Right(value),
+			"F" => Command::Forward(value),
+			_ => panic!("Unsupported command: {:?}", cmd),
 		})
-		.collect::<Box<[Command]>>();
+}
 
-	#[derive(Debug, Clone)]
-	struct Pos {
-		x: isize,
-		y: isize,
-		angle: isize,
-	}
+fn part_one() -> usize {
+	let (mut x, mut y, mut angle) = (0, 0, 90isize);
 
-	let mut pos = Pos {
-		x: 0,
-		y: 0,
-		angle: 90,
-	};
-
-	for cmd in commands.iter() {
-
-		let last = pos.clone();
-
-		match cmd {
-			Command::North(value) => pos.y += *value,
-			Command::South(value) => pos.y -= *value,
-			Command::East(value) => pos.x += *value,
-			Command::West(value) => pos.x -= *value,
-			Command::Left(value) => pos.angle -= *value,
-			Command::Right(value) => pos.angle += *value,
-			Command::Forward(value) => {
-				let angle = pos.angle.rem_euclid(360);
-				match angle {
-					0 => pos.y += value,
-					90 => pos.x += value,
-					180 => pos.y -= value,
-					270 => pos.x -= value,
-					_ => panic!("Fuck it: {}", angle),
-				}
-			}
+	for cmd in parse_input() {
+		match (cmd, angle) {
+			(Command::North(value), _) | (Command::Forward(value), 0) => y += value,
+			(Command::South(value), _) | (Command::Forward(value), 180) => y -= value,
+			(Command::East(value), _) | (Command::Forward(value), 90) => x += value,
+			(Command::West(value), _) | (Command::Forward(value), 270) => x -= value,
+			(Command::Left(value), _) => angle = (angle - value).rem_euclid(360),
+			(Command::Right(value), _) => angle = (angle + value).rem_euclid(360),
+			_ => unreachable!(),
 		}
 	}
 
-	(pos.x.abs() + pos.y.abs()) as usize
+	(x.abs() + y.abs()) as usize
 }
 
 fn part_two() -> usize {
-	let commands = include_str!("../input/input.txt")
-		.lines()
-		.map(|line| {
+	let (mut ship_x, mut ship_y) = (0, 0);
+	let (mut waypoint_x, mut waypoint_y) = (10, 1);
 
-			let (cmd, value) = line.split_at(1);
-			let value = value.parse::<isize>().unwrap();
-
-			match cmd {
-				"N" => Command::North(value),
-				"S" => Command::South(value),
-				"E" => Command::East(value),
-				"W" => Command::West(value),
-				"L" => Command::Left(value),
-				"R" => Command::Right(value),
-				"F" => Command::Forward(value),
-				_ => panic!("Unsupported command: {:?}", cmd),
-			}
-		})
-		.collect::<Box<[Command]>>();
-
-	#[derive(Debug, Clone)]
-	struct Pos {
-		x: isize,
-		y: isize,
-	}
-
-	let mut pos = Pos {
-		x: 0,
-		y: 0,
-	};
-
-	let mut waypoint = Pos {
-		x: 10,
-		y: 1,
-	};
-
-	for cmd in commands.iter() {
-		let last = pos.clone();
-
+	for cmd in parse_input() {
 		match cmd {
-			Command::North(value) => waypoint.y += *value,
-			Command::South(value) => waypoint.y -= *value,
-			Command::East(value) => waypoint.x += *value,
-			Command::West(value) => waypoint.x -= *value,
-			Command::Left(value) => {
-				let turns = value / 90;
-
-				for _ in 0..turns {
-					let y = waypoint.y;
-					waypoint.y = waypoint.x;
-					waypoint.x = -y;
-				}
+			Command::North(value) => waypoint_y += value,
+			Command::South(value) => waypoint_y -= value,
+			Command::East(value) => waypoint_x += value,
+			Command::West(value) => waypoint_x -= value,
+			Command::Left(value) => for _ in 0..(value / 90) {
+				std::mem::swap(&mut waypoint_x, &mut waypoint_y);
+				waypoint_x = -waypoint_x
 			},
-			Command::Right(value) => {
-				let turns = value / 90;
-
-				for _ in 0..turns {
-					let y = waypoint.y;
-					waypoint.y = -waypoint.x;
-					waypoint.x = y;
-				}
+			Command::Right(value) => for _ in 0..(value / 90) {
+				std::mem::swap(&mut waypoint_x, &mut waypoint_y);
+				waypoint_y = -waypoint_y;
 			},
 			Command::Forward(value) => {
-				pos.x += waypoint.x * value;
-				pos.y += waypoint.y * value;
+				ship_x += waypoint_x * value;
+				ship_y += waypoint_y * value;
 			}
 		}
-
-		// println!("{:?} => {:#?} => {:#?}", cmd, last, pos);
-		// println!("{:?}", waypoint);
 	}
 
-	(pos.x.abs() + pos.y.abs()) as usize
+	(ship_x.abs() + ship_y.abs()) as usize
 }
