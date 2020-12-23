ca503242f2a934e19ee9b5fc9ca2fd967c6deac7
Jezza <jezzadabomb@gmail.com>
2020-12-23 16:19:38 +0100

    Day 23 - Part One and Two.
    Speed up Day 22.

--- 2020/aoc22/src/main.rs
+++ 2020/aoc22/src/main.rs
@@ -3,23 +3,26 @@
 #![feature(array_value_iter)]
 
 use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
-
-use itertools::Itertools;
+use std::hash::{Hash, Hasher};
 
 use helper::measure;
 use helper::time;
-use std::hash::{Hash, Hasher};
 
 const INPUT: &str = include_str!("../input/input.txt");
 
 fn main() {
-	println!("Part One: {}", measure!(part_one()));
-	println!("Part Two: {}", measure!(part_two()));
-	// println!("Part One: {}", time!(part_one()));
-	// println!("Part Two: {}", time!(part_two()));
+	// println!("Part One: {}", measure!(part_one()));
+	// println!("Part Two: {}", measure!(part_two()));
+	println!("Part One: {}", time!(part_one()));
+	println!("Part Two: {}", time!(part_two()));
 }
 
-type Card = u16;
+// test part_one()                          	bench:	       8081 ns/iter (+/- 697)
+// Part One: 32162
+// test part_two()                          	bench:	    3802625 ns/iter (+/- 364415)
+// Part Two: 32534
+
+type Card = u8;
 type Deck = Vec<Card>;
 
 fn parse_input() -> (Deck, Deck) {
@@ -38,101 +41,114 @@ fn parse_input() -> (Deck, Deck) {
 	)
 }
 
-fn part_one() -> impl std::fmt::Display {
-	let (mut player_one, mut player_two) = parse_input();
 
-	while !player_one.is_empty() && !player_two.is_empty() {
-		let left = player_one.remove(0);
-		let right = player_two.remove(0);
-
-		if left > right {
-			player_one.push(left);
-			player_one.push(right);
-		} else {
-			player_two.push(right);
-			player_two.push(left);
-		}
-	}
-
-	let winner = if player_two.is_empty() {
-		player_one
-	} else {
-		player_two
-	};
-
-	winner.into_iter()
-		.rev()
-		.enumerate()
-		.map(|(i, v)| (i as u16 + 1) * v)
-		.sum::<u16>()
+enum RoundWinner {
+	PlayerOne(Card, Card),
+	PlayerTwo(Card, Card),
+	Default,
 }
 
-fn play_recursive_round(mut player_one: Deck, mut player_two: Deck) -> (Deck, Deck) {
+fn play_game<'a>(player_one: &'a mut Deck, player_two: &'a mut Deck, round: impl Fn(&mut Deck, &mut Deck, &mut HashSet<u64>) -> RoundWinner) -> &'a mut Deck {
 	let mut played = HashSet::new();
 
 	while !player_one.is_empty() && !player_two.is_empty() {
-
-		let already_played = {
-			let mut hasher = std::collections::hash_map::DefaultHasher::new();
-			player_one.hash(&mut hasher);
-			player_two.hash(&mut hasher);
-			let hash = hasher.finish();
-
-			!played.insert((hash, hash))
-		};
-
-		if already_played {
-			return (player_one, Vec::new());
-		}
-
-		let left = player_one.remove(0);
-		let right = player_two.remove(0);
-
-		let left_wins = if player_one.len() < left as usize || player_two.len() < right as usize {
-			left > right
-		} else {
-			let player_one = player_one.iter()
-				.copied()
-				.take(left as usize)
-				.collect();
-			let player_two = player_two.iter()
-				.copied()
-				.take(right as usize)
-				.collect();
-			let (_, winner_two) = play_recursive_round(player_one, player_two);
-			winner_two.is_empty()
-		};
-
-		if left_wins {
-			player_one.push(left);
-			player_one.push(right);
-		} else {
-			player_two.push(right);
-			player_two.push(left);
+		match round(player_one, player_two, &mut played) {
+			RoundWinner::PlayerOne(left, right) => {
+				player_one.push(left);
+				player_one.push(right);
+			}
+			RoundWinner::PlayerTwo(left, right) => {
+				player_two.push(left);
+				player_two.push(right);
+			}
+			RoundWinner::Default => {
+				player_two.clear();
+				return player_one;
+			},
 		}
 	}
 
 	if player_two.is_empty() {
-		(player_one, Vec::new())
+		player_one
 	} else {
-		(Vec::new(), player_two)
+		player_two
+	}
+}
+
+fn standard_round(left: &mut Deck, right: &mut Deck, _: &mut HashSet<u64>) -> RoundWinner {
+	let left = left.remove(0);
+	let right = right.remove(0);
+	if left > right {
+		RoundWinner::PlayerOne(left, right)
+	} else {
+		RoundWinner::PlayerTwo(right, left)
+	}
+}
+
+fn part_one() -> impl std::fmt::Display {
+	let (mut player_one, mut player_two) = parse_input();
+
+	play_game(&mut player_one, &mut player_two, standard_round)
+		.iter()
+		.copied()
+		.rev()
+		.enumerate()
+		.map(|(i, v)| (i + 1) * v as usize)
+		.sum::<usize>()
+}
+
+fn recursive_round(player_one: &mut Deck, player_two: &mut Deck, played: &mut HashSet<u64>) -> RoundWinner {
+	let already_played = {
+		let mut hasher = std::collections::hash_map::DefaultHasher::new();
+		player_one.hash(&mut hasher);
+		player_two.hash(&mut hasher);
+		let hash = hasher.finish();
+
+		!played.insert(hash)
+	};
+
+	if already_played {
+		return RoundWinner::Default;
+	}
+
+	let left = player_one.remove(0);
+	let right = player_two.remove(0);
+
+	let left_wins = if player_one.len() < left as usize || player_two.len() < right as usize {
+		left > right
+	} else {
+		let mut player_one = player_one.iter()
+			.take(left as usize)
+			.copied()
+			.collect::<Deck>();
+		let mut player_two = player_two.iter()
+			.take(right as usize)
+			.copied()
+			.collect::<Deck>();
+
+		if player_one.iter().max().unwrap() > player_two.iter().max().unwrap() {
+			true
+		} else {
+			play_game(&mut player_one, &mut player_two, recursive_round);
+			player_two.is_empty()
+		}
+	};
+
+	if left_wins {
+		RoundWinner::PlayerOne(left, right)
+	} else {
+		RoundWinner::PlayerTwo(right, left)
 	}
 }
 
 fn part_two() -> impl std::fmt::Display {
 	let (mut player_one, mut player_two) = parse_input();
+	let winner = play_game(&mut player_one, &mut player_two, recursive_round);
 
-	let (player_one, player_two) = play_recursive_round(player_one, player_two);
-
-	let winner = if player_two.is_empty() {
-		player_one
-	} else {
-		player_two
-	};
-
-	winner.into_iter()
+	winner.iter()
+		.copied()
 		.rev()
 		.enumerate()
-		.map(|(i, v)| (i as u16 + 1) * v)
-		.sum::<u16>()
+		.map(|(i, v)| (i + 1) * v as usize)
+		.sum::<usize>()
 }
---
+++ 2020/aoc23/src/main.rs
@@ -0,0 +1,90 @@
+use helper::measure;
+use helper::time;
+
+const INPUT: &str = include_str!("../input/input.txt");
+
+fn main() {
+	println!("Part One: {}", measure!(part_one()));
+	// println!("Part Two: {}", measure!(part_two()));
+	// println!("Part One: {}", time!(part_one()));
+	println!("Part Two: {}", time!(part_two()));
+}
+
+macro_rules! wrapping_sub {
+    ($expr:expr, $max:expr) => {{
+    	let result = $expr;
+    	if result == 1 {
+    		$max
+    	} else {
+    		result - 1
+    	}
+    }};
+}
+
+fn setup_input(length: usize) -> (usize, Vec<usize>) {
+	let input: Box<[usize]> = INPUT.bytes()
+		.map(|c| (c - b'0') as usize)
+		.collect();
+
+	let mut data = vec![0; length + 1];
+
+	let start = input[0];
+
+	let mut current = start;
+	for i in 1..length {
+		let next = if i < input.len() {
+			input[i]
+		} else {
+			i + 1
+		};
+		data[current] = next;
+		current = next;
+	}
+	data[current] = start;
+
+	(start, data)
+}
+
+fn play_game(length: usize, rounds: usize) -> Vec<usize> {
+	let (start, mut list) = setup_input(length);
+
+	let mut current = start;
+	for _ in 0..rounds {
+		let a = list[current];
+		let b = list[a];
+		let c = list[b];
+
+		let mut dest = wrapping_sub!(current, length);
+		while dest == a || dest == b || dest == c {
+			dest = wrapping_sub!(dest, length)
+		}
+
+		list[current] = list[c];
+		list[c] = list[dest];
+		list[dest] = a;
+		current = list[current];
+	}
+
+	list
+}
+
+fn part_one() -> impl std::fmt::Display {
+	let list = play_game(9, 100);
+
+	let mut answer = 0;
+
+	let mut current = 1;
+	while current < 9 {
+		let value = list[current];
+		answer = (answer * 10) + value;
+		current = value;
+	}
+
+	answer
+}
+
+fn part_two() -> impl std::fmt::Display {
+	let list = play_game(1_000_000, 10_000_000);
+
+	list[1] * list[list[1]]
+}
