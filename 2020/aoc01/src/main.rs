4f4e58044660ae6b188013a1b8f55755c1bf3df4
Jezza <jezzadabomb@gmail.com>
2021-11-28 15:04:24 +0100

    Improve Big O performance.

--- 2020/aoc01/src/main.rs
+++ 2020/aoc01/src/main.rs
@@ -6,7 +6,8 @@ fn main() {
 }
 
 fn try_main() -> Result<(), Box<dyn std::error::Error>> {
-	let numbers = read_numbers()?;
+	let mut numbers = read_numbers()?;
+	numbers.sort_unstable();
 
 	if let Some((l, r)) = find_pair(&numbers) {
 		println!("Part One: {:>4} * {:>4}        = {}", l, r, l * r);
@@ -24,30 +25,37 @@ fn try_main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn find_pair(input: &[u32]) -> Option<(u32, u32)> {
-	for (l_i, l) in input.iter().enumerate() {
-		for (r_i, r) in input.iter().enumerate() {
-			if r_i > l_i && *l + *r == 2020 {
-				return Some((*l, *r));
-			}
-		}
-	}
-	None
+	find_match(input, 2020)
 }
 
 fn find_triple(input: &[u32]) -> Option<(u32, u32, u32)> {
-	for (l_i, l) in input.iter().enumerate() {
-		for (m_i, m) in input.iter().enumerate() {
-			for (r_i, r) in input.iter().enumerate() {
-				if r_i > m_i && m_i > l_i && *l + *m + *r == 2020 {
-					return Some((*l, *m, *r));
-				}
-			}
+	for num in input {
+		let sum = 2020 - *num;
+		if let Some(factors) = find_match(input, sum) {
+			return Some((*num, factors.0, factors.1))
 		}
 	}
 	None
 }
 
-fn find_match() -> () {
+fn find_match(input: &[u32], sum: u32) -> Option<(u32, u32)> {
+	// a + b = sum
+	// sum - a = b
+
+	for num in input {
+		let num = *num;
+		if sum < num {
+			break;
+		}
+
+		let b = sum - num;
+
+		if let Ok(_) = input.binary_search(&b) {
+			return Some((num, b));
+		}
+	}
+
+	None
 }
 
 fn read_numbers() -> Result<Vec<u32>, std::num::ParseIntError> {
