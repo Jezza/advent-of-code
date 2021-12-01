5c8492bc2b7c0e34fd96c53c0cac253633133880
Jezza <jezzadabomb@gmail.com>
2021-12-01 09:36:30 +0100

    2021 - Day 01

--- 2021/day-01/src/main.rs
+++ 2021/day-01/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(array_windows)]
+
 use commons::*;
 
 const INPUT: &str = {
@@ -12,10 +14,31 @@ fn main() {
 	println!("Part Two: {}", part_two());
 }
 
-fn part_one() -> u64 {
-	0u64
+fn part_one() -> u16 {
+	INPUT.lines()
+		.filter_map(|line| line.parse::<u16>().ok())
+		.fold((0u16, None), |(count, previous), item| {
+			let count = previous.filter(|previous| *previous < item)
+				.map(|_| count + 1)
+				.unwrap_or(count);
+
+			(count, Some(item))
+		}).0
 }
 
 fn part_two() -> u64 {
-	0u64
+	let values: Vec<_> = INPUT.lines()
+		.filter_map(|line| line.parse::<u16>().ok())
+		.collect();
+
+	values.array_windows::<3>()
+		.fold((0, None), |(count, previous), values| {
+			let sum: u16 = values.iter().sum();
+
+			let count = previous.filter(|previous| *previous < sum)
+				.map(|_| count + 1)
+				.unwrap_or(count);
+
+			(count, Some(sum))
+		}).0
 }
