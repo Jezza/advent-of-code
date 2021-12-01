159b84fabe52266e01a72dbe772e772e4ab3f41b
Jezza <jezzadabomb@gmail.com>
2021-12-01 19:43:06 +0100

    Still trying to suss out a good structure.

--- 2021/day-01/src/main.rs
+++ 2021/day-01/src/main.rs
@@ -7,38 +7,35 @@ const INPUT: &str = {
 };
 
 fn main() {
-	measure!(part_one());
-	println!("Part One: {}", part_one());
+	println!("Part One: {} == {}", measure!(part_one()), 1301);
+	println!("Part Two: {} == {}", measure!(part_two()), 1346);
+}
 
-	measure!(part_two());
-	println!("Part Two: {}", part_two());
+fn read_input() -> impl Iterator<Item = u16> {
+	INPUT.lines()
+		.filter_map(|line| line.parse::<u16>().ok())
+}
+
+fn solve_input(input: impl Iterator<Item = u16>) -> u16 {
+	input.fold((0, None), |(count, previous), item| {
+		let count = previous.filter(|previous| *previous < item)
+			.map(|_| count + 1)
+			.unwrap_or(count);
+
+		(count, Some(item))
+	}).0
 }
 
 fn part_one() -> u16 {
-	INPUT.lines()
-		.filter_map(|line| line.parse::<u16>().ok())
-		.fold((0u16, None), |(count, previous), item| {
-			let count = previous.filter(|previous| *previous < item)
-				.map(|_| count + 1)
-				.unwrap_or(count);
-
-			(count, Some(item))
-		}).0
+	solve_input(read_input())
 }
 
-fn part_two() -> u64 {
-	let values: Vec<_> = INPUT.lines()
-		.filter_map(|line| line.parse::<u16>().ok())
+fn part_two() -> u16 {
+	let values: Vec<_> = read_input()
 		.collect();
 
-	values.array_windows::<3>()
-		.fold((0, None), |(count, previous), values| {
-			let sum: u16 = values.iter().sum();
+	let transformed = values.array_windows::<3>()
+		.map(|values| values.iter().sum::<u16>());
 
-			let count = previous.filter(|previous| *previous < sum)
-				.map(|_| count + 1)
-				.unwrap_or(count);
-
-			(count, Some(sum))
-		}).0
+	solve_input(transformed)
 }
