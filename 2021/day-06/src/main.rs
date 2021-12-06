02b883e007bf4e89db9f12ffc9196fc4f750c2ea
Jezza <jezzadabomb@gmail.com>
2021-12-06 15:22:47 +0100

    2021 - Day 06

---
+++ 2021/day-06/src/main.rs
@@ -0,0 +1,55 @@
+use commons::*;
+
+fn main() {
+	const TEST_1: &str = include_str!("../input/test-1.txt");
+	const INPUT: &str = include_str!("../input/input.txt");
+
+	aoc(part_one,
+		vec![
+			(TEST_1, 5934),
+			(INPUT, 390923),
+		],
+	);
+	aoc(part_two,
+		vec![
+			(TEST_1, 26984457539),
+			(INPUT, 1749945484935),
+		],
+	);
+}
+
+fn solve(input: &str, days: u16) -> u64 {
+	const EMPTY: [u64; 9] = [0; 9];
+
+	let mut counts = EMPTY;
+
+	input.split(",")
+		.filter_map(|v| v.parse::<u8>().ok())
+		.for_each(|v| counts[v as usize] += 1);
+
+	for _ in 0..days {
+		counts = counts.into_iter()
+			.enumerate()
+			.fold(EMPTY, |mut counts, (i, v)| {
+				if i == 0 {
+					counts[8] += v;
+					counts[6] += v;
+				} else {
+					counts[i as usize - 1] += v;
+				}
+				counts
+			});
+	}
+
+	counts.into_iter()
+		.map(|v| v as u64)
+		.sum::<u64>()
+}
+
+fn part_one(input: &str) -> u64 {
+	solve(input, 80)
+}
+
+fn part_two(input: &str) -> u64 {
+	solve(input, 256)
+}
